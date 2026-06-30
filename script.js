679a35344322a5fb0cfc144fa29ef8ab8d4f8c2b
Valeripol <157285012+Valeripol@users.noreply.github.com>
2026-06-30 17:37:09 +0100

    Initial portfolio website

---
+++ script.js
@@ -0,0 +1,12 @@
+const burger = document.getElementById("burger");
+const nav = document.getElementById("nav");
+
+burger.addEventListener("click", () => {
+  nav.classList.toggle("active");
+});
+
+document.querySelectorAll(".nav a").forEach((link) => {
+  link.addEventListener("click", () => {
+    nav.classList.remove("active");
+  });
+});
